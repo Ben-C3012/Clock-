1f9c592d6b389cc9964e4fca815af2a0804cc7c7
Ben Cosmin <94783151+Ben-C3012@users.noreply.github.com>
2022-05-17 15:45:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// Main Clock UI 
+const hourHeader = document.querySelector('.hours')
+const minHeader = document.querySelector('.min')
+const secHeader = document.querySelector('.sec')
+
+// Second Header 
+const dayOfWeek = document.querySelector('.day')
+const dayOfMonth = document.querySelector('.number')
+const monthString = document.querySelector('.month')
+
+
+
+
+// console.log(hour , min);
+
+
+
+setInterval(() => {
+
+    // Today 
+    const now = new Date()
+
+    // Get Time 
+    const hour = now.getHours()
+    const min = now.getMinutes()
+    const sec = now.getSeconds()
+
+
+    // Display Time
+    hourHeader.textContent = String(hour).padStart(2, 0)
+    minHeader.textContent = String(min).padStart(2, 0)
+    secHeader.textContent = String(sec).padStart(2, 0)
+
+    // Get And Display Date of Month  
+    const dayNumber = now.getDate()
+    dayOfMonth.textContent = String(dayNumber).padStart(2, 0)
+
+
+    //  Get And Display Month and day of week  
+    const dateArr = now.toDateString().split(' ')
+    dayOfWeek.textContent = dateArr[0] 
+    monthString.textContent = dateArr[1]
+
+
+
+
+
+
+}, 1000);
+
+
+const now = new Date()
+const dateArr = now.toDateString().split(' ')
+console.log(dateArr);
+
